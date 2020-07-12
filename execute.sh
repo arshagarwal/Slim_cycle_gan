814f90d75fb1f5273a1f14121b88624c0e0f24d2
Arsh <arshagarwal.18@gmail.com>
2020-07-13 04:00:26 +0530

    added flicker_data

--- execute.sh
+++ execute.sh
@@ -5,5 +5,6 @@ if [ "$1" == "-h" ]; then
    echo 'provide the name of the checkpoints file'
 fi
 git clone https://github.com/arshagarwal/Slim_cycle_gan.git
-mv  /contents/drive/My Drive/$1   /content/Slim_cycle_gan
-unzip /content/Slim_cycle_gan/$1 
+mv  '/content/drive/My Drive/'$1   /content/Slim_cycle_gan
+unzip /content/Slim_cycle_gan/$1
+mv -r /content/content/pytorch-CycleGAN-and-pix2pix/checkpoints   /content/Slim_cycle_gan
